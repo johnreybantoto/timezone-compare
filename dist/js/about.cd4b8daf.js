abd776dd140351c6255e13e5c929ff34b843d092
johnreybantotoMllrDev <johnrey.bantoto@mllrdev.com>
2022-03-19 11:58:39 +0800

    add dist folder

---
+++ dist/js/about.cd4b8daf.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var o=n("7a23"),a={class:"about"},c=Object(o["createElementVNode"])("h1",null,"Timezones comparer app built with Vue.js",-1),r=Object(o["createElementVNode"])("p",null,"Made with ♥ by John Rey Bantoto (johnreybantoto@gmail.com)",-1),l=[c,r];function b(e,t){return Object(o["openBlock"])(),Object(o["createElementBlock"])("div",a,l)}var u=n("6b0d"),i=n.n(u);const p={},s=i()(p,[["render",b]]);t["default"]=s}}]);
+//# sourceMappingURL=about.cd4b8daf.js.map
